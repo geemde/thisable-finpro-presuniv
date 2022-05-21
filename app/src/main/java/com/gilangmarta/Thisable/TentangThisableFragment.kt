c648cf7324661a833cfec3ad47b540df392e0b8c
Gilang Martadinata <50565813+geemde@users.noreply.github.com>
2022-05-21 22:26:42 +0700

    add tentangthisable fragment xml and kt

---
+++ app/src/main/java/com/gilangmarta/Thisable/TentangThisableFragment.kt
@@ -0,0 +1,22 @@
+package com.gilangmarta.Thisable
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gilangmarta.Thisable.databinding.FragmentTentangthisableBinding
+
+class TentangThisableFragment: Fragment() {
+    private var fragmentTentangthisableBinding: FragmentTentangthisableBinding? = null
+    private val binding get() = fragmentTentangthisableBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentTentangthisableBinding = FragmentTentangthisableBinding.inflate(inflater,container,false)
+        return fragmentTentangthisableBinding?.root
+    }
+}
